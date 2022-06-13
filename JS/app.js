57c09e97473c7c58d6b0ba28a1718afd8695c40f
PANPAN24 <mizael.valadez.5@gmail.com>
2022-06-13 11:56:17 -0500

    Ya consume la api y muestra los productos en una card, pero las imagenes estan harcodeadas

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,45 @@
-const URL_BASE = 'https://aresurbanwearback.000webhostapp.com/api/'
+
+let api =[];
+let cardContainer = document.querySelector("#cardContainer");
+
+const url = 'https://aresurbanwearback.000webhostapp.com/api/articulos'
 
 
-
-const testFetch = () => {
-    fetch(`${URL_BASE}clientes`)
-        .then(response => response.json())
-        .then(data => console.log(data));
+async function obtenerDatos(){
+    await fetch(url)
+    .then(datos => datos.json())
+    .then(datos => api.push( ...datos))
+    console.log(api)
+    imprimirCarta(api)
 }
+
+let imprimirCarta = (array) => {
+    array.forEach(articulo => {
+        cardContainer.innerHTML += `
+            
+     <div class="col">
+     <div class="card shadow-sm">
+
+         <img src="IMG/PROD/Gorra pescadora diseño canabis/principal.jpeg" 
+         width="350" height="300"></img>
+         <div class="card-body">
+          <h5 class="card-text">${articulo.descripcionArticulo}</h5>
+          <p class="card-text">${articulo.precioArticulo}</p>
+          <div class="d-flex justify-content-between align-items-center">
+             <div class="btn-group">
+         
+                 <a href="" class="btn btn-primary">Detalles</a>
+         
+               </div>
+                <a href="" class="btn btn-success">Agregar al carrito</a>
+            </div>
+        </div>
+     </div>
+  </div>
+        
+        `    
+    
+    }); 
+}
+
+obtenerDatos();
